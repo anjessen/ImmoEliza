c7caa9b34bbe3b341232cc3ee43b22000ae11070
Delahay Joann <delahay.joann@gmail.com>
2020-11-26 16:48:52 +0100

    resolve index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,10 +14,10 @@ document.getElementById('terrace_no').addEventListener('click', function(){
 
 
 
-const type_of_property = document.getElementsByClassId("type_of_property");
-const number_of_rooms = document.getElementsByClassId("Number_of_rooms");
-const house_area = document.getElementsByClassId("house-area");
-const inputCityPC = document.getElementsByClassId("inputCityPC")
+const type_of_property = document.getElementById("type_of_property");
+const number_of_rooms = document.getElementById("Number_of_rooms");
+const house_area = document.getElementById("house-area");
+const inputCityPC = document.getElementById("inputCityPC")
 
 document.getElementById('type_of_property').addEventListener('click', function(){
     if (type_of_property == NULL || number_of_rooms == NULL || house_area == NULL || inputCityPC == NULL)
